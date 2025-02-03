5aaaa24ed50bff4332269132dc8556f81e486785
Pablo <0001070522@senaimgaluno.com.br>
2025-02-03 09:01:14 -0300

    Adicionado imagens e novos produtos; correção no tema (escuro/claro)

--- PadariaDelicioPão/script.js
+++ PadariaDelicioPão/script.js
@@ -1,22 +1,23 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const modoTemaBtn = document.getElementById('modo-tema');
-    const body = document.body;
+document.addEventListener("DOMContentLoaded", function () {
+  const modoTemaBtn = document.getElementById("modo-tema");
+  const body = document.body;
 
-    // Verificar se o usuário já tem uma preferência salva
-    if (localStorage.getItem('darkMode') === 'enabled') {
-        body.classList.add('dark-mode');
-        modoTemaBtn.textContent = '🌙';
+  if (localStorage.getItem("darkMode") === "enabled") {
+    body.classList.add("dark-mode");
+    modoTemaBtn.textContent = "🌙";
+  } else {
+    modoTemaBtn.textContent = "🌞";
+  }
+
+  modoTemaBtn.addEventListener("click", function () {
+    body.classList.toggle("dark-mode");
+
+    if (body.classList.contains("dark-mode")) {
+      localStorage.setItem("darkMode", "enabled");
+      modoTemaBtn.textContent = "🌙";
+    } else {
+      localStorage.setItem("darkMode", "disabled");
+      modoTemaBtn.textContent = "🌞";
     }
-
-    modoTemaBtn.addEventListener('click', function() {
-        body.classList.toggle('dark-mode');
-        
-        if (body.classList.contains('dark-mode')) {
-            localStorage.setItem('darkMode', 'enabled');
-            modoTemaBtn.textContent = '🌙';
-        } else {
-            localStorage.setItem('darkMode', 'disabled');
-            modoTemaBtn.textContent = '🌞';
-        }
-    });
+  });
 });
